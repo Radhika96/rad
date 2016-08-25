330c947d840d860cc49d1f6c32787e65fc98b0cd
Radhika96 <radponnan@gmail.com>
2016-08-25 10:26:21 +0530

    Update Nsum.java

--- Nsum.java
+++ Nsum.java
@@ -4,8 +4,8 @@ public class Nsum
 {
 	public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-  int n=sc.nextInt();
-  int sum=0;
+  	int n=sc.nextInt();
+  	int sum=0;
 	for(int i=0;i<=n;i++)
 	sum=sum+i;
 	System.out.println(sum);
