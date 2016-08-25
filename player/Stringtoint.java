0657deac8e6cc85245385f3f221c0ff0b5ca390f
Radhika96 <radponnan@gmail.com>
2016-08-25 13:40:33 +0530

    Create Stringtoint.java

---
+++ player/Stringtoint.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Stringtoint
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+	  String str=sc.nextLine();
+		Integer i=Integer.valueOf(str);
+		System.out.println(i);
+	  sc.close();
+	}
+}
