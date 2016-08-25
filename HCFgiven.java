3f8229b9e5d766fa44104e0410ff715c70ba41ff
Radhika96 <radponnan@gmail.com>
2016-08-25 11:58:23 +0530

    Update and rename Gcdgiven.java to HCFgiven.java

--- HCFgiven.java
+++ HCFgiven.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import java.util.*;
-public class Gcdgiven {
-	public static int GCD(int a, int b) {
+public class HCFgiven {
+	public static int HCF(int a, int b) {
 	    if (b == 0) return a;
-	    else return (GCD (b, a % b));
+	    else return (HCF (b, a % b));
 	}
 	public static void main(String[] args) {
 	  Scanner sc=new Scanner(System.in);
 	  int x,y,gcd;          
 		x=sc.nextInt();
 		y=sc.nextInt();
-		gcd=GCD(x,y);
+		gcd=HCF(x,y);
 		System.out.println(gcd);
 }
