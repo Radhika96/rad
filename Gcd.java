8d094f5e049c6a0dde27449f37c9d4706563524e
Radhika96 <radponnan@gmail.com>
2016-08-25 11:52:22 +0530

    Create Gcd.java

---
+++ Gcd.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Gcd {
+	public static int GCD(int a, int b) {
+	    if (b == 0) return a;
+	    else return (GCF (b, a % b));
+	}
+	public static void main(String[] args) {
+		int x=9,y=24,gcd;          
+		gcd=GCD(x,y);
+		System.out.println(gcd);
+}
