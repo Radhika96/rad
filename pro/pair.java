7665f351353d4779a42a0288930b2d8c80c64a79
Radhika96 <radponnan@gmail.com>
2016-08-31 20:57:06 +0530

    Create pair.java

---
+++ pro/pair.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class pair
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();
+    int m=1;
+    for(int i=0;i<2*num;i++)
+    {
+    for(int j=m;j<2*num;j++)
+    {
+      if(i==j)
+        continue;
+      else
+      {
+    		 System.out.println(i+""+j);	
+    	}
+    }
+    m+=1;
+    }
+  sc.close(); 
+  }
+}
